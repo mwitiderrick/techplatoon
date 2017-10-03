68195e949cbd2e22a0d5c2db74926946c38b137f
Tshepom59 <tshepo.morabane@meltwater.org>
2017-10-03 13:55:36 +0000

    corrections on task

--- task.py
+++ task.py
@@ -6,21 +6,21 @@ class Task:
         self.comment = comment
 
     def add_comment(self):
-    	return(self.comment)
+        return(self.comment)
 
     def delete_comment(self):
-    	if comment in self.comment:
-    		del comment
+        if comment in self.comment:
+            del comment
 
     def __repr__(self):
-    	return "{} is due on {} at {}".format(self.name, self.date, self.time)
-    	
+        return "{} is due on {} at {}".format(self.name, self.date, self.time)
+    
 if __name__ == "__main__":
-	TName = input("Assign Name: ")
-	TDate = input("Assign Date: ")
-	TTime = input("Assign Time: ")
-	TComment = input("Assign Comment: ")
+    task_name = input("Assign Name: ")
+    task_date = input("Assign Date: ")
+    task_time = input("Assign Time: ")
+    task_comment = input("Assign Comment: ")
 
-	New_Task = Task(TName, TDate,TTime, TComment)
-	print(New_Task)
+    New_Task = Task(task_name, task_date,task_time, task_comment)
+    print(New_Task)
 
