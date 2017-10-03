fd871441c6aaf2f8d974ae158c8be728fdf4de53
dareo <dare.odumade@yahoo.com>
2017-10-03 16:18:01 +0100

    added task edit&delete methods

--- boardlist.py
+++ boardlist.py
@@ -6,11 +6,13 @@ class BoardList:
         self.tasks = tasks
         self.name = name
 
+
     def __repr__(self):
         return """
         Boardlist: {}
         {}
         """.format(self.name,self.tasks)
+
 
     def add_task(self, task):
         if isinstance(task, Task):
@@ -18,16 +20,24 @@ class BoardList:
         else:
             print("that does not look like a task")
 
+
     def edit_task(self):
-            pass
+        task_to_delete = input('enter the task to delete: ')
+        for element in tasks:
+            if element == task_to_delete:
+                new_task = input('enter a new task')
+                element = new_task
+                print(tasks)
+
 
     def delete_task(self, tasks):
-         n = int(input("Enter the position"))
-         if n >= len(tasks):
-            del tasks[n]
+         n = int(input("Enter the position:"))
+         if n >= (len(tasks)+1):
+            del tasks[n-1]
             print(tasks)
+         else:
+             print('Error')
 
 
 
-userstory1 = BoardList('dd')
-
+userstory1 = BoardList()
