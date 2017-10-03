03a661b5856718a64925b983f9d3e5bf71a481d4
malenge254 <techiechic12.34@gmail.com>
2017-10-03 15:50:52 +0000

    "add members to board"

--- board.py
+++ board.py
@@ -2,17 +2,18 @@ from user import User
 from boardlist import BoardList
 
 
-class Board(User):
-    def __init__(self, name, color, boardlists=[]):
+class Board():
+    def __init__(self, name, color, boardlists=[], users=[]):
         self.name = name
         self.color = color
         self.boardlists = boardlists
+        self.users = users
 
     def __repr__(self):
         return """
         Board: {}
-        {}
-        """.format(self.name, self.boardlists)
+        {} {}
+        """.format(self.name, self.boardlists, self.users)
 
     def change_board_name(self, newname):
         self.name = newname
@@ -26,9 +27,12 @@ class Board(User):
         if self.name == searchname:
             return self.name
 
-    def add_team_member(self, newmember):
-        if newmember not in User(self.fullname):
-            self.fullname.append(newmember)
+    def add_member(self, member):
+        if isinstance(member, User): 
+            if member in self.users:
+                self.users.append(member)
+            else:
+                print("user does not exist")
     
     def add_boardlist(self, boardlist):
         if isinstance(boardlist, BoardList):
@@ -40,5 +44,6 @@ class Board(User):
 if __name__ == '__main__':
     firstboard = Board("hypothesis", "red")
     firstboard.add_boardlist(BoardList("To do"))
+    firstboard.add_member("gf")
     print(firstboard)
     
